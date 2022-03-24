3ad7c65718a08104711e1d6f9cc70892cbd6432c
jestorDev <torrele.09@gmail.com>
2022-03-24 14:07:19 -0500

    Await until all data is inserted

--- sqlize.js
+++ sqlize.js
@@ -1,7 +1,7 @@
 
-const  data  = require( "./data.json")
+const data = require("./data.json")
 
-const { Sequelize, DataTypes , Model} = require('sequelize');
+const { Sequelize, DataTypes, Model } = require('sequelize');
 const { ListFormat } = require("typescript");
 
 
@@ -11,33 +11,37 @@ const sequelize = new Sequelize('postgres://postgres:123456@0.0.0.0:5432/disneyD
 const queryInterface = sequelize.getQueryInterface();
 
 async function initTables() {
-    async function  initTable(properties, name) {
-        console.log("Creating Table " , name );
-        await queryInterface.createTable(name , properties)
-        
+    async function initTable(properties, name) {
+        console.log("Creating Table ", name);
+        await queryInterface.createTable(name, properties)
+
     }
-    function insertData(dataArr , model) {
-        console.log(dataArr.length);
+    async function insertData(dataArr, model) {
+        console.log("Len:", dataArr.length);
+        let promises = []
         for (const elem of dataArr) {
-            model.create(elem).then(elem => {
-                console.log(elem.ID);
-            })     
+            promises.push(model.create(elem))
         }
+
+        await Promise.all(promises)
+
+
+
     }
-    const charProperties ={
+    const charProperties = {
         ID: {
             type: DataTypes.BIGINT,
             allowNull: false,
             autoIncrement: true,
             primaryKey: true
-    
+
         },
         name: {
             type: DataTypes.STRING(200)
         },
         image: {
             type: DataTypes.STRING(3000)
-    
+
         },
         age: {
             type: DataTypes.INTEGER.UNSIGNED
@@ -45,15 +49,15 @@ async function initTables() {
         weitgh: {
             type: DataTypes.DOUBLE
         },
-    
+
         history: {
             type: DataTypes.TEXT
         },
-    
+
     }
-    
-    const genProperties =  {
-    
+
+    const genProperties = {
+
         ID: {
             type: DataTypes.BIGINT,
             allowNull: false,
@@ -66,10 +70,10 @@ async function initTables() {
         image: {
             type: DataTypes.STRING(3000)
         },
-    
+
     }
     const movProperties = {
-    
+
         ID: {
             type: DataTypes.BIGINT,
             allowNull: false,
@@ -92,24 +96,47 @@ async function initTables() {
                 max: 5.0,
             }
         },
-    
+
     }
 
     const tableprops = {
-        sequelize,freezeTableName: true,
+        sequelize, freezeTableName: true,
     }
-    const Character = sequelize.define("Character", charProperties , tableprops  )
-    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"); // true
-    console.log(Character === sequelize.models.Character); // true
-    const Genre = sequelize.define("Genre", genProperties , tableprops  )
-    const Movies_Series =sequelize.define("Movies_Series", movProperties , tableprops  )
+    const Character = sequelize.define("Character", charProperties, tableprops)
+    console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log(Character === sequelize.models.Character);
+    const Genre = sequelize.define("Genre", genProperties, tableprops)
+    const Movies_Series = sequelize.define("Movies_Series", movProperties, tableprops)
+
+
+
+    //Movies_Series.belongsToMany(Genre ,{through:'Movie_Genre'})
+    //Genre.belongsToMany(Movies_Series ,{through:'Movie_Genre'})
+
+    Character.belongsToMany(Movies_Series, { through: 'Character_Movies' })
+    Movies_Series.belongsToMany(Character, { through: 'Character_Movies' })
+
+    //Character.belongsToMany(Character ,{through:'Associated_Characters'})
 
 
     await sequelize.sync({ force: true });
-    
-    insertData(data.characterArr , Character)
-    insertData(data.genreArr , Genre)
-    insertData(data.movieArr , Movies_Series)
+
+    insertData(data.characterArr, Character)
+    insertData(data.genreArr, Genre)
+    insertData(data.movieArr, Movies_Series)
+
+    const result = await Character.findOne({
+        where: { name: 'Simba' }
+    });
+    console.log(result);
+    //setTimeout(
+    //    async ()=>{
+    //        
+    //    }
+    //    , 10000
+    //)
+
+
 }
 
 
